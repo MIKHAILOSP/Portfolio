044ffff3ca7593aef57a5e996ebf9b99a149d667
Mikhailosp <logan4684@ya.ru>
2019-10-26 18:15:15 +0400

    refoctor: rebuttons

--- script2.js
+++ script2.js
@@ -2,10 +2,10 @@
 const leftButton = document.getElementById("btn-left");
 const rightButton = document.getElementById("btn-right");
 leftButton.addEventListener('click',()=>{
-    const content=document.getElementById("content");
+    let content=document.getElementById("content");
     content.className="slide-content0";
 })
 rightButton.addEventListener('click',()=>{
-    const content=document.getElementById("content");
+    let content=document.getElementById("content");
     content.className="slide-content1";
 })
