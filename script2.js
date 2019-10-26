f439cf3cfcb1a00e452ac395aac85feb19726ceb
Mikhailosp <logan4684@ya.ru>
2019-10-27 00:31:22 +0400

    refactor: infinity slider

--- script2.js
+++ script2.js
@@ -1,11 +1,32 @@
 // import "./style.css";
 const leftButton = document.getElementById("btn-left");
 const rightButton = document.getElementById("btn-right");
+let slides = document.getElementsByClassName("slide");
 leftButton.addEventListener('click',()=>{
     let content=document.getElementById("content");
+    let btn=document.getElementById("btn-left");
+    let btn1=document.getElementById("btn-right");
+    if (content.className == "slide-content0"){
+    content.className="slide-content1";
+    btn.className="leftbtn1";
+    btn1.className="rightbtn1";
+    } else {
     content.className="slide-content0";
+    btn.className="leftbtn";
+    btn1.className="rightbtn";
+    }
 })
 rightButton.addEventListener('click',()=>{
     let content=document.getElementById("content");
+    let btn=document.getElementById("btn-left");
+    let btn1=document.getElementById("btn-right");
+    if (content.className == "slide-content1"){
+        btn.className="leftbtn";   
+        btn1.className="rightbtn";
+        content.className = "slide-content0";
+    } else {
     content.className="slide-content1";
+    btn.className="leftbtn1";
+    btn1.className="rightbtn1";
+    }
 })
